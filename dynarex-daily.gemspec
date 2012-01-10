d085263a76db817f45bf1a7abf995a0dcc53fec5
Your Name <james@r0bertson.co.uk>
2012-01-10 16:38:23 +0000

    1st commit

---
+++ dynarex-daily.gemspec
@@ -0,0 +1,7 @@
+Gem::Specification.new do |s|
+  s.name = 'dynarex-daily'
+  s.version = '0.1.0'
+  s.summary = 'dynarex-daily'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/**/*.rb']
+end
---
+++ lib/dynarex-daily.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+# file: dynarex-daily.rb
+
+require 'dynarex'
+require 'fileutils'
+
+class DynarexDaily < Dynarex
+
+  def initialize(stringx=nil)
+
+    @filename = 'dynarexdaily.xml'
+    super(stringx) if stringx
+
+    if File.exist?(@filename) then 
+      super(@filename)
+      if !summary[:date].empty? and \
+          Time.parse(summary[:date]).day != Time.now.day then
+        archive_file
+        create_file
+      end  
+    else
+      create_file
+    end
+  end
+
+  private
+  
+  def create_file()
+    
+    FileUtils.touch @filename
+    
+    initialize('entries[date]/entry(time, desc)')
+    summary[:date] = Time.now.to_s
+    save @filename
+  end
+
+  def archive_file()
+    dir = 'days'
+    FileUtils.mkdir dir unless File.exist? dir
+    FileUtils.mv(@filename, "%s/%s" % [dir, Time.now.strftime("d%d%m%y.xml")])
+  end
+
+end
