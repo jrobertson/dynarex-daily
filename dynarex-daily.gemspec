8a0ffffd1449589cdef6b0c173ba91ee5c7de85d
James Robertson <james@r0bertson.co.uk>
2023-01-30 10:10:48 +0000

    bug fix: Now works with Ruby 3.2.0. Changed File.exists? to File.exist?

--- dynarex-daily.gemspec
+++ dynarex-daily.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'dynarex-daily'
-  s.version = '0.5.1'
+  s.version = '0.6.0'
   s.summary = 'A Dynarex flavoured log file for humans which is archived daily'
   s.authors = ['James Robertson']
   s.files = Dir['lib/dynarex-daily.rb']
-  s.add_runtime_dependency('dynarex', '~> 1.9', '>=1.9.6')
+  s.add_runtime_dependency('dynarex', '~> 1.10', '>=1.10.0')
   s.signing_key = '../privatekeys/dynarex-daily.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/dynarex-daily.rb
+++ lib/dynarex-daily.rb
@@ -6,12 +6,12 @@ require 'dynarex'
 require 'rxfileio'
 
 
-class DynarexDaily < Dynarex
+class DynarexDaily < Dynarex  
   include RXFileIOModule
-
-  def initialize(stringx=nil, dir_archive: :days, xslt: '',
+ 
+  def initialize(stringx=nil, dir_archive: :days, xslt: '', 
                  filename: 'dynarexdaily.xml', debug: false)
-
+    
     @dir_archive = dir_archive
 
     @filename = filename
@@ -20,30 +20,31 @@ class DynarexDaily < Dynarex
     @debug = debug
 
     puts 'DynarexDaily::initialize stringx: ' + stringx.inspect  if @debug
-
+    
     if stringx then
       s, type = RXFReader.read(stringx)
       @filename = stringx if type == :file or type == :dfs
     end
-
+    
     puts 'DynarexDaily::initialize @filename: ' + @filename.inspect  if @debug
-
-    if FileX.exists?(@filename) then
-
+    
+    if FileX.exist?(@filename) then
+      
+      puts 'file exists!' if @debug
       super @filename, debug: debug
-
+      
       if !summary[:date].empty? and \
           Date.parse(summary[:date]) != Date.today then
-
+        
         archive_file Date.parse(summary[:date])
         create_file
-      end
-
+      end  
+      
     else
       puts 'before super: stringx: ' + stringx.inspect if @debug
       super( stringx || @schema , debug: debug)
       puts 'after super' if @debug
-      @delimiter = ' # '
+      @delimiter = ' # '      
       create_file
     end
 
@@ -51,37 +52,39 @@ class DynarexDaily < Dynarex
   end
 
   def create(h)
+    
+    puts 'Date.today: ' + Date.today.inspect if @debug
 
     if !summary[:date].empty? and \
         Date.parse(summary[:date]) != Date.today then
-
+      
       archive_file Date.parse(summary[:date])
       create_file
-    end
-
+    end      
+    
     super(h)
   end
-
-
+    
+  
   def save(filename=@filename, options={})
 
     puts 'inside DynarexDaily::save() filename: ' + filename.inspect if @debug
     super(filename, options)
 
   end
-
+  
   def schema=(s)
     super(s.sub(/^\w+(?=\/)/,'\0[date]'))
     summary[:date] = Date.today.to_s
-    summary[:order] = 'descending'
+    summary[:order] = 'descending'    
   end
 
   private
-
+  
   def create_file()
-
-    puts 'inside DynarexDaily::create_file' if @debug
-
+    
+    puts 'inside DynarexDaily::create_file' if @debug   
+    
     openx(@schema)
     summary[:date] = Date.today.to_s
     summary[:order] = 'descending'
@@ -91,26 +94,29 @@ class DynarexDaily < Dynarex
 
   def archive_file(t)
 
-    puts 'inside DynarexDaily::archive_file' if @debug
-
+    puts 'inside DynarexDaily::archive_file' if @debug   
+    
     dir, file = if @dir_archive == :days then
+      puts 'days archive' if @debug
       ['days', t.strftime("d%d%m%y.xml")]
     else
+      puts 'other archive' if @debug
       [t.strftime("%Y/%b/%d").downcase, 'index.xml']
     end
 
+    puts 'about to mkdir ' + dir.inspect if @debug
     FileX.mkdir_p dir unless FileX.exist? dir
-
+    
     if @debug then
       puts 'inside DynarexDaily::archive_file '
       puts '  @filename: %s' % [@filename]
       puts '  dir: %s; file: %s' % [dir, file]
       puts '  FileX: %s' % [dir, FileX.pwd]
     end
-
+    
     FileX.mv(@filename, "%s/%s" % [dir, file])
     puts 'inside DynarexDaily::archive_file after FileX.mv' if @debug
-
+    
   end
 
 end
